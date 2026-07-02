815380b5878eb596ae181c7e15b2213009987250
diegofdg <diegofdg@gmail.com>
2026-07-02 06:11:01 -0300

    seccion 04-conceptos en angular

---
+++ seccion_04/ejercicios/02-EventBinding-UAC/src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { NuevoComponenteComponent } from "./nuevo-componente/nuevo-componente.component";
+import { ComponenteEnLineaComponent } from "./componente-en-linea/componente-en-linea.component";
+import { InterpolacionComponent } from "./interpolacion/interpolacion.component";
+import { PadreComponent } from './padre/padre.component';
+import { HijoComponent } from "./hijo/hijo.component";
+import { MostrarMensajeComponent } from "./mostrar-mensaje/mostrar-mensaje.component";
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, NuevoComponenteComponent, ComponenteEnLineaComponent, InterpolacionComponent, PadreComponent, HijoComponent, MostrarMensajeComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  titulo = 'Universidad Angular';
+}
---
+++ seccion_04/ejercicios/03-PropertyBinding-UAC/src/app/app.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { NuevoComponenteComponent } from "./nuevo-componente/nuevo-componente.component";
+import { ComponenteEnLineaComponent } from "./componente-en-linea/componente-en-linea.component";
+import { InterpolacionComponent } from "./interpolacion/interpolacion.component";
+import { PadreComponent } from './padre/padre.component';
+import { HijoComponent } from "./hijo/hijo.component";
+import { MostrarMensajeComponent } from "./mostrar-mensaje/mostrar-mensaje.component";
+import { ReplicadorComponent } from "./replicador/replicador.component";
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, NuevoComponenteComponent, ComponenteEnLineaComponent, InterpolacionComponent, PadreComponent, HijoComponent, MostrarMensajeComponent, ReplicadorComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  titulo = 'Property Binding en Angular';
+}
