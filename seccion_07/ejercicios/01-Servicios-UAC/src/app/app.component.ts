f85510664517e75646571be914ec48ef89b2c603
diegofdg <diegofdg@gmail.com>
2026-07-05 06:01:48 -0300

    seccion 07-servicios en angular

---
+++ seccion_07/ejercicios/01-Servicios-UAC/src/app/app.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { NuevoComponenteComponent } from "./nuevo-componente/nuevo-componente.component";
+import { ComponenteEnLineaComponent } from "./componente-en-linea/componente-en-linea.component";
+import { InterpolacionComponent } from "./interpolacion/interpolacion.component";
+import { PadreComponent } from './padre/padre.component';
+import { MostrarMensajeComponent } from "./mostrar-mensaje/mostrar-mensaje.component";
+import { ReplicadorComponent } from "./replicador/replicador.component";
+import { SaludarComponent } from "./saludar/saludar.component";
+import { ComponenteIfComponent } from "./componente-if/componente-if.component";
+import { AgregarTareaComponent } from "./agregar-tarea/agregar-tarea.component";
+import { ComponenteForComponent } from "./componente-for/componente-for.component";
+import { ViewChildComponent } from "./view-child/view-child.component";
+import { MensajeService } from './mensaje.service';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, NuevoComponenteComponent, ComponenteEnLineaComponent, InterpolacionComponent, PadreComponent, MostrarMensajeComponent, ReplicadorComponent, SaludarComponent, ComponenteIfComponent, AgregarTareaComponent, ComponenteForComponent, ViewChildComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  titulo = 'Servicios en Angular';
+
+  mensaje: string;
+
+  constructor(mensajeService: MensajeService){
+    this.mensaje = mensajeService.obtenerMensaje();
+  }
+}
---
+++ seccion_07/ejercicios/04-EmitirMensajesServicio-ListadoProductos-UAC/src/app/producto/producto.component.ts
@@ -0,0 +1,20 @@
+import { Component, Input } from '@angular/core';
+import { Producto } from './producto.model';
+import { ProductoService } from '../producto.service';
+
+@Component({
+  selector: 'app-producto',
+  standalone: true,
+  imports: [],
+  templateUrl: './producto.component.html',
+  styleUrl: './producto.component.css'
+})
+export class ProductoComponent {
+  @Input() producto!: Producto;
+
+  constructor(private productoService: ProductoService){}
+
+  emitirDetalleProducto(){
+    this.productoService.detalleProductoEmitter.emit(this.producto);
+  }
+}
